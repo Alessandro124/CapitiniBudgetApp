79aba28123636ae42b7262c00fe68d431d9ff839
offfahad <mughalfahad544@gmail.com>
2023-09-24 21:56:28 +0500

    money-calculated

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,3 +1,4 @@
+import 'package:budget_app/models/expense_model.dart';
 import 'package:flutter/material.dart';
 import 'package:budget_app/models/category_model.dart';
 
@@ -12,6 +13,13 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpend = 0;
+    widget.category.expenses.forEach((Expense expense) {
+      totalAmountSpend += expense.cost;
+    });
+    final double amountLeft = widget.category.maxAmount - totalAmountSpend;
+    final double percent = amountLeft / widget.category.maxAmount;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
@@ -34,15 +42,19 @@ class _CategoryScreenState extends State<CategoryScreen> {
               height: 250.0,
               width: double.infinity,
               decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: const [
-                    BoxShadow(
-                        color: Colors.black12,
-                        offset: Offset(0, 2),
-                        blurRadius: 6.0),
-                  ],
-                ),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(0, 2),
+                      blurRadius: 6.0),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}'),
+              ),
             ),
           ],
         ),
