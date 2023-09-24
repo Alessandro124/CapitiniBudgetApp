eda51a14964f1c41aa2ce312395cf64d37000686
offfahad <mughalfahad544@gmail.com>
2023-09-24 21:51:07 +0500

    radio-progressbar

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:budget_app/models/category_model.dart';
 
 class CategoryScreen extends StatefulWidget {
-  const CategoryScreen({required this.category,super.key});
+  const CategoryScreen({required this.category, super.key});
 
   final Category category;
   @override
@@ -12,6 +12,41 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(widget.category.name),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+            iconSize: 30.0,
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.all(20.0),
+              height: 250.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0, 2),
+                        blurRadius: 6.0),
+                  ],
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
