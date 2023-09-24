d17dbea47c6e321a0d2025c999e491c5e282a92a
offfahad <mughalfahad544@gmail.com>
2023-09-24 19:07:20 +0500

    silver-app-bar-done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,7 +11,29 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            leading: IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {},
+              iconSize: 30.0,
+            ),
+            flexibleSpace: const FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text('Simple Budget'),
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                iconSize: 30.0,
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
